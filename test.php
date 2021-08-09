e443aba4f32baf100cf80e9edc39e31e534f65d5
Uriy Jykov <ldomofon-sericel@rambler.ru>
2021-08-09 20:14:32 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,40 @@
+<?php
+
+//создаем класс 
+class Cat 
+{ 
+public $name;
+
+//Поведение:
+public function meow ()
+{
+echo "Мяу-Мяу";
+}
+
+public function eat ()
+{
+echo "Попил молока";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
