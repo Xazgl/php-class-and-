512239d12207535a513af93248f0501a91279b9a
Uriy Jykov <ldomofon-sericel@rambler.ru>
2021-08-09 20:59:17 +0300

    first commit

--- test.php
+++ test.php
@@ -1,36 +1,50 @@
 <?php
 
 //создаем класс 
+//Характеристики: Имя
 class Cat 
 { 
 public $name;
 
+public function __construct($newName)
+{
+$this->name=$newName;
+echo "Родилась кошечка" . $this->name . "\n";
+}
+
+// очитска памяти зарезервированой 
+public function __destruct()
+{
+    echo $this->name . "удалили\n";
+}
+
 //Поведение:
+
+//1
 public function meow ()
 {
 echo "Мяу-Мяу";
 }
 
+//2
 public function eat ()
 {
 echo "Попил молока";
 }
+}
+//Создание объекта + инициалзицая
+$cat = new Cat('Мурзик');
 
+//Инициализация объекта
+//$cat->name = 'Мурзик';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//использование объекта (цикл  for на 10 мяу и еда)
+for($i=0; $i<10; $i++)
+{
+    $cat->meow ();
+}
+$cat->eat();
+ 
 
 
 
